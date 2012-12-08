d410afdf57c268a99e698ff8f1b03ab1dd9f66b6
Nis Sarup <nis@sarup.dk>
2012-12-08 16:33:59 +0100

    Scene loading and device found.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 
 using namespace std;
 using namespace rw::common;
+using namespace rw::loaders;
+using namespace rw::models;
 
 int main(int argc, char** argv) {
     cout << "Program startet." << endl;
-
+    
+    cout << "Loading scene." << endl;
+    string wcFile = "/Users/tamen/Documents/Archive/Skole/SDU/7Semester/ROB/Exercises/Mandatory2/KUKA_KR120_scene/KukaKr120_CDH_downMill.wc.xml";
+    WorkCell::Ptr wc = WorkCellLoader::load(wcFile);
+    
+    string deviceName = "KukaKr120";
+    cout << "Finding device: " << deviceName << "." << endl;
+    Device::Ptr device = wc->findDevice(deviceName);
+    if (device == NULL) {
+		cout << "Device: " << deviceName << " not found!" << endl;
+        return 1;
+	}
+    
 	cout << "Program done." << endl;
 	return 0;
 }
